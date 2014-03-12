5daee74ad705eb35713c455b270c1d788f462a08
hornyP <ubunicorn@googlemail.com>
2014-03-12 22:59:47 +0100

    http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 __author__ = 'pepo'
 
 from flask import Flask
-from Flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
+
